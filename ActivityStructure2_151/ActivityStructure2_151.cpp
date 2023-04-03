b6ff46c57f387807de6b95a08ff9e226472372e9
hilal555 <muhamadhilal04@gmail.com>
2023-04-03 15:01:56 +0700

    Output ke layat

---
+++ ActivityStructure2_151/ActivityStructure2_151.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct AlamatDetail
+{
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string Nim;
+	string Nama;
+	AlamatDetail Alamat;
+	int Umur;
+};
+
+int main() {
+
+	Mahasiswa mhs;
+
+	cout << " Masukkan nim : ";
+	cin >> mhs.Nim;
+	cout << "Masukkan nama : ";
+	cin >> mhs.Nama;
+	cout << "Alamat : ";
+	cout << "\tNama desa : ";
+	cin >> mhs.Alamat.desa;
+	cout << "\tNama kota : ";
+	cin >> mhs.Alamat.kota;
+	cout << "Masukkan umur : ";
+	cin >> mhs.Umur;
+
+	cout << "\nNim : " << mhs.Nim;
+	cout << "\nNama : " << mhs.Nama;
+	cout << "\ndesa : " << mhs.Alamat.desa;
+	cout << "\nkota : " << mhs.Alamat.kota;
+}
