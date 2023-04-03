f70b3f1baa65cd6119ddb47264709c3ddb85a6af
hilal555 <muhamadhilal04@gmail.com>
2023-04-03 14:43:02 +0700

    Membuat jenis data

--- ActivityStructure_151/ActivityStructure_151.cpp
+++ ActivityStructure_151/ActivityStructure_151.cpp
@@ -15,5 +15,27 @@ int main()
 	mhs1.nim = "20220140151";
 	mhs1.nama = "Hilal";
 	mhs1.alamat = "Jogja";
-	mhs.umur = 19;
+	mhs1.umur = 19;
+
+	cout << "Masukkan NIM : ";
+	cin >> mhs2.nim;
+	cout << "Masukkan Nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukkan Alamat : ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan Umur : ";
+	cin >> mhs2.umur;
+
+	cout << "\nNIM : " << mhs1.nim;
+	cout << "\nNama : " << mhs1.nama;
+	cout << "\nAlamat : " << mhs1.alamat;
+	cout << "\nUmur : " << mhs1.umur;
+
+	cout << "\nNIM : " << mhs2.nim;
+	cout << "\nNama : " << mhs2.nama;
+	cout << "\nAlamat : " << mhs2.alamat;
+	cout << "\nUmur : " << mhs2.umur;
+
+	
 }
+ 
