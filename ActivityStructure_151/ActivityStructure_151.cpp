2b9f5d19eafd0647da1d8c4d5fa43c8e864f0dc1
hilal555 <muhamadhilal04@gmail.com>
2023-04-03 14:22:57 +0700

    Add project files.

---
+++ ActivityStructure_151/ActivityStructure_151.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20220140151";
+	mhs.nama
+}
